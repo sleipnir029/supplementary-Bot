2d93ba776e4466c46b941f982feca1ba686a485a
romo1romo <52344836+sleipnir029@users.noreply.github.com>
2020-05-02 21:27:04 +0600

    added keys

--- supplementaryBot.py
+++ supplementaryBot.py
@@ -3,3 +3,13 @@ import pandas as pd
 import wikipedia
 import tweepy
 import random
+
+
+
+# Keys for authentication
+CONSUMER_KEY = 'ItdSIzZCtPi2jxeuenxuHtEEp'
+CONSUMER_SECRET = 'dLS8xt8DSsuVfxSfOdZvrM86IxvasolYZ9wU8ZDD0BcuXU1Nyl'
+ACCESS_KEY = '1249506273831161856-MgdRnM0Q7PRQTtkZ6dwIurQJZTC1nl'
+ACCESS_SECRET = 'lhP9AjMrPM7GdrzQg8Dlj9YfFTaE6cwyYWFgClhruzh2R'
+
+
