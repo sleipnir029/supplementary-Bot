f07e77fc849c5031bed939dd96c617226ce03fc6
romo1romo <52344836+sleipnir029@users.noreply.github.com>
2020-05-02 21:34:00 +0600

    searching articles in wikipedia

--- supplementaryBot.py
+++ supplementaryBot.py
@@ -14,10 +14,23 @@ ACCESS_SECRET = 'lhP9AjMrPM7GdrzQg8Dlj9YfFTaE6cwyYWFgClhruzh2R'
 
 # Retriving trends data from Google (global)
 pytrend = TrendReq()
-df = pytrend.trending_searches()                # Search for 
-alpha = random.randint(0, 5)                    # Choosing a random number of trend
+df = pytrend.trending_searches()                                            # Search for 
+alpha = random.randint(0, 5)                                                # Choosing a random number of trend
 print('Trend number: ', alpha)
-trend = df.values[alpha]                        # Passes on numero uno
+trend = df.values[alpha]                                                    # Passes on numero uno
 listToStr = ' '.join(map(str, trend))           
 strToList = list(listToStr.split(' '))          
 print(strToList)
+
+
+# Searching Wikipedia for appropriate articles
+searched_titles = []
+for i in strToList:
+    retrived_titles = wikipedia.search(i, results=3, suggestion=False)      # value of "results" dictates ammount of searched items
+    searched_titles.append(retrived_titles)
+print(searched_titles)
+
+retrived_titles_modified = sum(searched_titles, [])                         # Joining list of lists
+print(retrived_titles_modified)
+randomized_title = random.choice(retrived_titles_modified)
+print('Random title:', randomized_title)
