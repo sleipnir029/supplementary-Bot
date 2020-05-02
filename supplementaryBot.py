ebda7889a7c923fa7b2c189cd6eac894b3da3d9e
romo1romo <52344836+sleipnir029@users.noreply.github.com>
2020-05-02 21:29:31 +0600

    retriving google data

--- supplementaryBot.py
+++ supplementaryBot.py
@@ -5,7 +5,6 @@ import tweepy
 import random
 
 
-
 # Keys for authentication
 CONSUMER_KEY = 'ItdSIzZCtPi2jxeuenxuHtEEp'
 CONSUMER_SECRET = 'dLS8xt8DSsuVfxSfOdZvrM86IxvasolYZ9wU8ZDD0BcuXU1Nyl'
@@ -13,3 +12,12 @@ ACCESS_KEY = '1249506273831161856-MgdRnM0Q7PRQTtkZ6dwIurQJZTC1nl'
 ACCESS_SECRET = 'lhP9AjMrPM7GdrzQg8Dlj9YfFTaE6cwyYWFgClhruzh2R'
 
 
+# Retriving trends data from Google (global)
+pytrend = TrendReq()
+df = pytrend.trending_searches()                # Search for 
+alpha = random.randint(0, 5)                    # Choosing a random number of trend
+print('Trend number: ', alpha)
+trend = df.values[alpha]                        # Passes on numero uno
+listToStr = ' '.join(map(str, trend))           
+strToList = list(listToStr.split(' '))          
+print(strToList)
