385b92377b832e0ce3f82c82799c9b0c458bfd31
Rakibuzzaman Rahat <52344836+sleipnir029@users.noreply.github.com>
2020-05-12 04:52:30 +0600

    tweet to twitter

--- supplementaryBot.py
+++ supplementaryBot.py
@@ -60,7 +60,7 @@ tweet = page_content + '...' + '\n' + page_url
 print(tweet)
 
 
-# Setting up tweet authentication
+# Setting up twitter authentication
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth, wait_on_rate_limit=True,
